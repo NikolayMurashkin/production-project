c65baab671bc262ac006d0f1d8ab9d73d4a73e9f
Nikolay <m.nikolay1990@gmail.com>
2023-06-29 12:52:49 +0700

    added dev server

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,26 @@
+import path from 'path';
+import webpack from 'webpack';
+import { buildDevServer, buildLoaders, buildPlugins, buildResolvers } from '..';
+import { BuildOptions } from './types/config';
+
+export function buildWebpackConfig(
+	options: BuildOptions
+): webpack.Configuration {
+	const { mode, paths, isDev } = options;
+	return {
+		mode: mode,
+		entry: paths.entry,
+		output: {
+			filename: '[name].[contenthash].js',
+			path: paths.build,
+			clean: true,
+		},
+		module: {
+			rules: buildLoaders(),
+		},
+		resolve: buildResolvers(),
+		plugins: buildPlugins(options),
+		devtool: isDev ? 'inline-source-map' : undefined,
+		devServer: isDev ? buildDevServer(options) : undefined,
+	};
+}
---
+++ config/index.ts
@@ -0,0 +1,5 @@
+export * from './build/buildPlugins';
+export * from './build/buildLoaders';
+export * from './build/buildResolvers';
+export * from './build/buildWebpackConfig';
+export * from './build/buildDevServer';
