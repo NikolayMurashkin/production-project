09ddda443dc7055e72aaaf0c30f01bf83a0e8f28
Nikolay <m.nikolay1990@gmail.com>
2024-04-11 12:24:29 +0700

    fix routes

--- src/pages/ArticleDetailsPage/index.ts
+++ src/pages/ArticleDetailsPage/index.ts
@@ -2,6 +2,6 @@ export {
     ArticleDetailsPageAsync as ArticleDetailsPage
 } from './ui/ArticleDetailsPage.async';
 
-export type { ArticleDetailsCommentsSchema } from './model/types/articleDetailsCommentsSchema';
+export type { ArticleDetailsCommentsSchema } from './model/types/ArticleDetailsCommentsSchema';
 export type { ArticleDetailsRecommendationsSchema } from './model/types/ArticleDetailsRecommendationsSchema';
 export type { ArticleDetailsPageSchema } from './model/types';
