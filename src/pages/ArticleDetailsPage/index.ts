91db00db686667dba9169e5c9429192c31d4e794
Nikolay <m.nikolay1990@gmail.com>
2024-04-11 12:30:38 +0700

    fix naming

--- src/pages/ArticleDetailsPage/index.ts
+++ src/pages/ArticleDetailsPage/index.ts
@@ -2,6 +2,6 @@ export {
     ArticleDetailsPageAsync as ArticleDetailsPage
 } from './ui/ArticleDetailsPage.async';
 
-export { ArticleDetailsCommentsSchema } from './model/types/ArticleDetailsCommentsSchema';
-export { ArticleDetailsRecommendationsSchema } from './model/types/ArticleDetailsRecommendationsSchema';
+export { ArticleDetailsCommentsSchema } from './model/types/articleDetailsCommentsSchema';
+export { ArticleDetailsRecommendationsSchema } from './model/types/articleDetailsRecommendationsSchema';
 export { ArticleDetailsPageSchema } from './model/types';
