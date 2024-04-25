fdaabe351541a37ba15c049358bcc16f75258ed7
Nikolay <m.nikolay1990@gmail.com>
2024-04-25 20:16:10 +0700

    add circular dependenct plugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,14 +1,18 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import webpack from 'webpack';
+import webpack, { WebpackPluginInstance } from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import CopyPlugin from 'copy-webpack-plugin';
 import { BuildOptions } from './types/config';
+import CircularDependencyPlugin from 'circular-dependency-plugin';
 
 export function buildPlugins({
-    paths, isDev, apiUrl, project,
-}: BuildOptions): webpack.WebpackPluginInstance[] {
+    paths,
+    isDev,
+    apiUrl,
+    project,
+}: BuildOptions): WebpackPluginInstance[] {
     const plugins = [
         new HtmlWebpackPlugin({
             template: paths.html,
@@ -24,19 +28,23 @@ export function buildPlugins({
             __PROJECT__: JSON.stringify(project),
         }),
         new CopyPlugin({
-            patterns: [
-                { from: paths.locales, to: paths.buildLocales },
-            ],
+            patterns: [{ from: paths.locales, to: paths.buildLocales }],
         }),
+        new CircularDependencyPlugin({
+            exclude: /node_modules/,
+            failOnError: true,
+        })
     ];
 
     if (isDev) {
         plugins.push(new ReactRefreshWebpackPlugin());
         plugins.push(new webpack.HotModuleReplacementPlugin());
-        plugins.push(new BundleAnalyzerPlugin({
-            openAnalyzer: false,
-        }));
+        plugins.push(
+            new BundleAnalyzerPlugin({
+                openAnalyzer: false,
+            })
+        );
     }
-
+    // @ts-ignore
     return plugins;
 }
